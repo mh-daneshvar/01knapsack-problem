12a15883e19298ec8afca4e6ad8bd1ec1525d101
mh-daneshvar <repoday.com@gmail.com>
2021-07-15 12:13:17 +0300

    feat(pack): implement Pack class
    - Pack is responsible for getting items and representing the solution
    - Pack is responsible about setting the strategy for resolving the problem

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
-import Pack from './src/pack';
+import ItemModel  from './src/models/item.model';
+import Pack       from './src/Pack'
+import Strategies from './src/strategies/strategies.enum'
 
-new Pack()
+// we take the same instance of the problem displayed in the image
+const items = [
+  new ItemModel('element 1', 13, 9072),
+  new ItemModel('element 2', 40, 3380),
+  new ItemModel('element 3', 10, 4315),
+  new ItemModel('element 4', 16, 3797),
+  new ItemModel('element 5', 36, 4681),
+  new ItemModel('element 6', 79, 4877),
+  new ItemModel('element 7', 45, 8180),
+  new ItemModel('element 8', 79, 1936),
+  new ItemModel('element 9', 64, 676)
+]
+
+Pack.pack(Strategies.TABULATION, items, 5200)
--- src/pack.ts
+++ src/pack.ts
@@ -1,8 +1,48 @@
+import ItemModel  from './models/item.model'
+import Context    from './strategies/context'
+import Strategies from './strategies/strategies.enum'
+
 export default class Pack {
-  constructor() {
-    console.clear();
-    console.log('------------------------>');
-    console.log('debugging:\n', JSON.stringify('we wanna pack everything in a better way...', null, 2));
-    console.log('------------------------>');
+
+  /**
+   *
+   * @param strategyName
+   * @param items
+   * @param capacity
+   */
+  public static pack(strategyName: Strategies, items: ItemModel[], capacity: number) {
+    // Validate the given inputs
+    Pack.validateInputs(strategyName, items, capacity);
+
+    // Execute the strategy
+    const strategy = new Context(items, capacity)
+    strategy.setStrategy(strategyName)
+
+    // Get the solution
+    const solution = strategy.executeStrategy()
+
+    // Do what you want
+    if (solution) {
+      solution.printInfo()
+    }
+  }
+
+  /**
+   *
+   * @param strategyName
+   * @param items
+   * @param capacity
+   * @private
+   */
+  private static validateInputs(strategyName: Strategies, items: ItemModel[], capacity: number) {
+    if (!strategyName) {
+      throw new Error('Invalid strategy!')
+    }
+    if (!items || items.length < 1) {
+      throw new Error('Invalid items! You should enter at least 1 item.')
+    }
+    if (!capacity || capacity < 0 || capacity > 100000) {
+      throw new Error('Invalid capacity! Capacity should be more than 0 and less than 100.')
+    }
   }
 }
